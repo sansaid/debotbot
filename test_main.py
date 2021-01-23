1404884a5e430a765ca362075edaa1701b49837d
Sanyia Saidova <sanyia.saidova@gmail.com>
2021-01-23 22:22:02 +0000

    Moving tests to its own directory

--- test_main.py
+++
@@ -1,5 +0,0 @@
-from unittest import TestCase
-from main import *
-
-def test_start_debate():
-    start_debate(update, context)
---
+++ tests/test_bot.py
@@ -0,0 +1,8 @@
+# import sys
+# import os
+
+# THIS_DIR = os.path.dirname(__file__)
+# sys.path.append(os.path.join(THIS_DIR, "../lib"))
+
+from debotbot import *
+from unittest import TestCase
